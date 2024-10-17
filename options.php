a4e0be1ace8276effa5c200f89dab381835124ed
aksel96 <jonhaksel@gmail.com>
2024-10-17 16:24:10 -0600

    clase 1712024 comit 1

---
+++ options.php
@@ -0,0 +1,10 @@
+<?php
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fruta_seleccionada = $_POST["fruta"];
+    echo"<h1>Seleccionaste la fruta : $fruta_seleccionada !</h1>";
+
+
+}
+
+?>
